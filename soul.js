b5211233c95c1b3e115949389ab9e186cde37a17
wtfijwaname <1902505035@qq.com>
2025-04-16 01:31:37 +0800

    Create soul.js

---
+++ soul.js
@@ -0,0 +1,16 @@
+// 适用于QuantumultX的响应体修改脚本
+const url = $request.url;
+if (url.includes('user/info/detail')) {
+    try {
+        let body = JSON.parse($response.body);
+        // 修改金币和总余额
+        body.data.goldBalance = 9999;      // 金币数值修改
+        body.data.totalBalance = 9999;     // 总余额数值修改
+        $done({ body: JSON.stringify(body) });
+    } catch (e) {
+        console.log(`脚本执行错误：${e}`);
+        $done({});
+    }
+} else {
+    $done({});
+}
