1a8d6547aad6148c1c8ee1a45dcc9675900fdca5
wtfijwaname <1902505035@qq.com>
2025-04-16 01:54:35 +0800

    Update soul.js

--- soul.js
+++ soul.js
@@ -10,8 +10,11 @@ if (url.includes('user/info/detail')) {
     try {
         let body = JSON.parse($response.body);
         // 修改金币和总余额
-        body.data.goldBalance = 9999;      // 金币数值修改
-        body.data.totalBalance = 9999;     // 总余额数值修改
+              // 金币数值修改
+       bady = {
+           "goldBalance" : 9999,
+           "totalBalance": 9999,
+       }     // 总余额数值修改
         $done({ body: JSON.stringify(body) });
     } catch (e) {
         console.log(`脚本执行错误：${e}`);
