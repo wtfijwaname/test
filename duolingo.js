c144b017c8675b2c5aacfc6c1df463228575f326
wtfijwaname <1902505035@qq.com>
2026-04-22 02:12:44 +0800

    更新 duolingo.js

--- duolingo.js
+++ duolingo.js
@@ -1,7 +1,19 @@
 [rewrite_local]
-^https?:\/\/ios-api\.duolingo\.com\/2017-06-30\/users\/\d+\?.*fields=subscriberLevel url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/duolingo/duolingo.js
-^https?:\/\/ios-api\.duolingo\.com\/2017-06-30\/users\/\d+\/available-features url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/duolingo/duolingo.js
-^https?:\/\/ios-api\.duolingo\.com\/2017-06-30\/batch url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/duolingo/duolingo.js
+# 基于您提供的真实URL结构
+# 您的URL: ^http://ios-api-2.duolingo.cn/...
+
+# 1. 匹配数字变化，域名变化
+^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3}\/.+users\/.+\?fields=subscriberLevel url script-response-body https://he2o.vercel.app/Resource/Script/Duo/level.js
+
+# 2. 注意：您提供的是http不是https，所以用https?匹配两种
+^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3}\/.+users\/.+\/available-features url script-response-body https://he2o.vercel.app/Resource/Script/Duo/features.js
+
+# 3. 批处理请求
+^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3}\/.+\/batch url script-response-body https://he2o.vercel.app/Resource/Script/Duo/batch.js
+
+# 4. 调试用：查看所有匹配的请求
+^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3} url script-response-body https://raw.githubusercontent.com/crossutility/Quantumult-X/master/sample-import-export/sample-response-body.js
 
 [mitm]
-hostname = ios-api.duolingo.com
+# 使用通配符匹配所有可能的域名
+hostname = ios-api-*.duolingo.*
