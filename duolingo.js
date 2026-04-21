3963d05152eae6c75a6f8637cf81afe0c3630c9b
wtfijwaname <1902505035@qq.com>
2026-04-22 02:01:14 +0800

    更新 duolingo.js

--- duolingo.js
+++ duolingo.js
@@ -1,20 +1,8 @@
 [rewrite_local]
-# ======== 多邻国高级版解锁 ========
-# 用户订阅等级解锁
-^https?:\/\/ios-api\.duolingo\.com\/2017-06-30\/users\/\d+(\?fields=|%3Ffields=)subscriberLevel url script-response-body https://he2o.vercel.app/Resource/Script/Duo/level.js
-
-# 功能解锁（新API模式）
-^https?:\/\/ios-api\.duolingo\.com\/2017-06-30\/users\/\d+\/available-features url script-response-body https://he2o.vercel.app/Resource/Script/Duo/features.js
-
-# 批处理请求解锁
-^https?:\/\/ios-api\.duolingo\.com\/2017-06-30\/batch url script-response-body https://he2o.vercel.app/Resource/Script/Duo/batch.js
-
-# 旧API兼容（如有）
-^https?:\/\/api-prod\.duolingo\.com\/2017-06-30\/(users\/\d+|batch) url script-response-body https://he2o.vercel.app/Resource/Script/Duo/batch.js
-
-# 超级会员解锁
-^https?:\/\/ios-api\.duolingo\.com\/2017-06-30\/users\/\d+\?fields=isSuperuser url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/icon/other/duolingo/duolingo-super.js
+# 调试规则1：捕获所有duolingo请求并显示
+^https?:\/\/.*\.duolingo\.com url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/box/rewrite/response-body.js
+^https?:\/\/.*\.duolingo\.net url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/box/rewrite/response-body.js
+^https?:\/\/.*\.duolingo\.org url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/box/rewrite/response-body.js
 
 [mitm]
-# 多邻国主域名
-hostname = ios-api.duolingo.com, api-prod.duolingo.com, *.duolingo.com
+hostname = *.duolingo.com, *.duolingo.net, *.duolingo.org
