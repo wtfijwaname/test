c1a9978a3fad79a2f4efca35656e3d8c676601a5
wtfijwaname <1902505035@qq.com>
2026-04-22 02:25:41 +0800

    更新 duolingo.js

--- duolingo.js
+++ duolingo.js
@@ -1,19 +1,6 @@
 [rewrite_local]
-# 基于您提供的真实URL结构
-# 您的URL: ^http://ios-api-2.duolingo.cn/...
-
-# 1. 匹配数字变化，域名变化
-^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3}\/.+users\/.+\?fields=subscriberLevel url script-response-body https://he2o.vercel.app/Resource/Script/Duo/level.js
-
-# 2. 注意：您提供的是http不是https，所以用https?匹配两种
-^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3}\/.+users\/.+\/available-features url script-response-body https://he2o.vercel.app/Resource/Script/Duo/features.js
-
-# 3. 批处理请求
-^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3}\/.+\/batch url script-response-body https://he2o.vercel.app/Resource/Script/Duo/batch.js
-
-# 4. 调试用：查看所有匹配的请求
-^https?:\/\/ios-api-\d+\.duolingo\.[a-z]{2,3} url script-response-body https://raw.githubusercontent.com/crossutility/Quantumult-X/master/sample-import-export/sample-response-body.js
-
+^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/.*\/users\/.*\?fields=subscriberLevel url script-response-body https://he2o.vercel.app/Resource/Script/Duo/level.js, tag=订购等级
+^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/.*\/users\/.*\/available-features.*$ url script-response-body https://he2o.vercel.app/Resource/Script/Duo/features.js, tag=订购配置
+^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/\d{4}-\d{1,2}-\d{1,2}\/batch url script-response-body https://he2o.vercel.app/Resource/Script/Duo/batch.js, tag=订购信息
 [mitm]
-# 使用通配符匹配所有可能的域名
-hostname = ios-api-*.duolingo.*
+hostname = ios-api-*.duolingo.*, api*.duolingo.*
